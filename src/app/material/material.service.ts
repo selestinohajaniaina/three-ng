d09f6d15071487f542ce89345ce02d173717d684
selestino <selestinoolivier@gmail.com>
2025-06-03 17:24:24 +0300

    @selestinohajaniaina #6 - fixed some light propreties

--- src/app/material/material.service.ts
+++ src/app/material/material.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BoxGeometry, BufferGeometry, Camera, Line, LineBasicMaterial, Mesh, MeshBasicMaterial, MeshLambertMaterial, Vector3, AmbientLight, SpotLight, PointLight, MeshPhysicalMaterial, MeshPhongMaterial, SpotLightHelper, PointLightHelper, PlaneGeometry, DoubleSide } from 'three';
-import { GLTFLoader, OrbitControls } from 'three/addons';
+import { BoxGeometry, BufferGeometry, Camera, Line, LineBasicMaterial, Mesh, MeshBasicMaterial, MeshLambertMaterial, Vector3, AmbientLight, SpotLight, PointLight, MeshPhysicalMaterial, MeshPhongMaterial, SpotLightHelper, PointLightHelper, PlaneGeometry, DoubleSide, Plane, PlaneHelper, RectAreaLight, SphereGeometry } from 'three';
+import { GLTFLoader, OrbitControls, RectAreaLightHelper } from 'three/addons';
 
 @Injectable({
   providedIn: 'root'
@@ -11,20 +11,32 @@ export class MaterialService {
 
   CubeBasic(x: number, y: number, z: number) {
     const cubeGeometry = new BoxGeometry(x, y, z);
-    const cubeMaterial = new MeshBasicMaterial({ color: 0x00ff00 });
+    const cubeMaterial = new MeshBasicMaterial({ color: 0xffff00 });
     return new Mesh(cubeGeometry, cubeMaterial);
   }
 
   CubeLambert(x: number, y: number, z: number) {
     const cubeGeometry = new BoxGeometry(x, y, z);
-    const cubeMaterial = new MeshLambertMaterial({ color: 0x00ff00 });
+    const cubeMaterial = new MeshLambertMaterial({ color: 0xffff00 });
     return new Mesh(cubeGeometry, cubeMaterial);
   }
 
   CubePhong(x: number, y: number, z: number) {
     const cubeGeometry = new BoxGeometry(x, y, z);
-    const cubeMaterial = new MeshPhongMaterial({ color: 0x00ff00 });
+    const cubeMaterial = new MeshPhongMaterial({ color: 0xffff00 });
     return new Mesh(cubeGeometry, cubeMaterial);
+  }
+
+  SphereBasic(r: number, ps: number, tl: number) {
+    const sphereGeo = new SphereGeometry(r, ps, tl);
+    const sphereMat = new MeshBasicMaterial({ color: 0x02ff00 });
+    return new Mesh(sphereGeo, sphereMat);
+  }
+
+  SpherePhong(r: number, ps: number, tl: number) {
+    const sphereGeo = new SphereGeometry(r, ps, tl);
+    const sphereMat = new MeshPhongMaterial({ color: 0xffff00 });
+    return new Mesh(sphereGeo, sphereMat);
   }
 
   Line() {
@@ -52,11 +64,10 @@ export class MaterialService {
   }
 
   AmbiantLight(x: number, y: number, z: number) {
-    const light = new AmbientLight(0xffffff, 10);
-    light.position.set(x, y, z);
-    light.lookAt(0, 0, 0);
-    light.castShadow = true;
-
+    const light = new AmbientLight(0x404040, 10);
+    // light.position.set(x, y, z);
+    // light.lookAt(0, 0, 0);
+    // light.castShadow = true;
     return light;
   }
 
@@ -79,12 +90,30 @@ export class MaterialService {
     return l;
   }
 
+  RectLight() {
+    const width = 10;
+    const height = 10;
+    const intensity = 1;
+    const rectLight = new RectAreaLight( 0xffffff, intensity,  width, height );
+    rectLight.position.set( 5, 5, 0 );
+    rectLight.lookAt( 0, 0, 0 );
+    const rectLightHelper = new RectAreaLightHelper( rectLight );
+    rectLight.add( rectLightHelper );
+    return rectLight;
+  }
+
   PlanGeometry(w: number, y: number) {
     const pl_g = new PlaneGeometry(w, y, 30, 30);
-    const pl_m = new MeshBasicMaterial({ color: 0x3f3f3f, side: DoubleSide});
+    const pl_m = new MeshLambertMaterial({ color: 'skyblue', side: DoubleSide});
     const pl = new Mesh(pl_g, pl_m);
+    pl.receiveShadow = true;
     pl.rotateX(-Math.PI/2);
     return pl;
+  }
+
+  Plan() {
+    const plane = new Plane( new Vector3( 10, 10, 0.2 ), 0 );
+    return new PlaneHelper( plane, 10, 0xffff00 );
   }
 
 }
--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Line, Mesh, PerspectiveCamera, Scene, Vector3 } from 'three';
+import { Color, Mesh, PerspectiveCamera, Scene, Vector3 } from 'three';
 import { WebGLRenderer } from 'three';
 import { MaterialService } from '../material/material.service';
 
@@ -24,20 +24,30 @@ export class SceneComponent {
     const { width, height } = sceneFrame.getBoundingClientRect();
     this.render.setSize(width, height);
 
-    this.camera.position.set(0, 10, 10);
+    this.camera.position.set(1, 5, 15);
     this.camera.lookAt(0, 0, 0);
     this.cube = this.material.CubeLambert(1, 1, 1);
-    this.cube.position.set(0, 3, 0);
+    this.cube.position.set(0, 1, 0);
 
     const orbitControle = this.material.OrbitControle(this.camera, this.render.domElement);
     orbitControle.update();
 
-    const ambLight = this.material.AmbiantLight(5, 5, 5);
+    const ambLight = this.material.AmbiantLight(0, 0, 0);
+    this.scene.add( ambLight );
+
+    const SpotLight = this.material.AmbiantLight(10, 10, 10);
+    SpotLight.lookAt(this.scene.position);
+    this.scene.add( SpotLight );
+
+    // const rectLight = this.material.RectLight();
+    // this.scene.add( rectLight );
 
     const plan = this.material.PlanGeometry(10, 10);
-    console.log(plan.rotation.x);
+    this.scene.add( plan );
+
+    // this.scene.background = new Color(0xffff00);
     
-    this.scene.add(this.cube, ambLight, plan);
+    this.scene.add(this.cube);
     this.render.setAnimationLoop( this.animate );
   }
 
